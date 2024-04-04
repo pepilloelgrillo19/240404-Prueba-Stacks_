cc54a3c1e9ec253cd24d693a27fe2d56ab8ff4f8
pepilloelgrillo19 <157523378+pepilloelgrillo19@users.noreply.github.com>
2024-04-04 15:29:53 +0200

    Initial Commit

---
+++ 240404 Prueba Stacks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  240404 Prueba Stacks
+//
+//  Created by Luis on 4/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 240404 Prueba Stacks/_40404_Prueba_StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  _40404_Prueba_StacksApp.swift
+//  240404 Prueba Stacks
+//
+//  Created by Luis on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct _40404_Prueba_StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
